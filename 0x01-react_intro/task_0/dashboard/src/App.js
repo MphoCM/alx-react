223ccd09730a9ca4a0e15a4a4c4d64b746c592b1
MphoCM <useradd@DESKTOP-5ESRLO7>
2024-04-05 23:48:00 +0200

    update

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// Define a functional component named App
+function App() {
+    // Return JSX elements representing a simple HTML structure
+    return (
+        <div>
+            {/* Render a heading with the text "Hello, World!" */}
+            <h1>Hello, World!</h1>
+            {/* Render a paragraph with the text "This is a JSX element." */}
+            <p>This is a JSX element.</p>
+        </div>
+    );
+}
+
+// Export the App component as the default export
+export default App;
