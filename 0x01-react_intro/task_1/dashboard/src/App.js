9f1d21e843da6d30901808197abfb47cfd655bbe
MphoCM <christian.mofokeng@gmail.com>
2024-04-06 03:36:46 +0200

    update

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// Define a functional component named App
+function App() {
+    // Return JSX elements representing a simple HTML structure
+    return (
+        <div> className=App>
+          <header className="App-header">
+            <img src="/path/to/holberton-logo.png" alt="Holberton Logo" />
+            <h1>School dashboard</h1>
+</header>
+<body className="App-body">
+<div>
+  <p>Login to access the full dashboard</p>
+  </div>
+  <body>
+    <footer className="App-footer">
+      <div>
+        <p>Login toaccess the full dashboard</p>
+        </div>
+        </body>
+        <footer className="App-footer".
+        <div>
+        <p>Copyright 2020 - Holberton School</p>
+        </div>
+    </footer>
+    </div>
+    );
+}
+
+// Export the App component as the default export
+export default App;
